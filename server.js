600c55326a27dd636be1b0333f92fbcde9e5c930
Nikhil Haspe <nick0416>
2022-06-03 22:45:29 +0530

    setup laravel-mix for bundle, scss, js, compilation

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 // Setting Template Engine
-app.use(express.static(path.join(__dirname, '/resources/views')));
+app.use(express.static('public'));
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
 

322ca89e731ab1896e55293bf7e907bfd9e5e97f
Nikhil Haspe <nick0416>
2022-06-04 10:48:11 +0530

    setup ejs layouts

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path');
+app.use(expressLayout);
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,10 +12,15 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
 
-app.listen(PORT, () => {
-    console.log(`Listening port ${PORT}`);
-});
-
+// ROUTES
 app.get('/', (req, res) => {
     res.render('home.ejs');
 });
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart.ejs');
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening port ${PORT}`);
+}) 

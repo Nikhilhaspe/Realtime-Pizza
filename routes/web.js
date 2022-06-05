08764449a9e8a3b62de0c9dcc341fe66b12b3e55
Nikhil Haspe <nick0416>
2022-06-05 15:14:50 +0530

    add change order status functionality for admin

--- routes/web.js
+++ routes/web.js
@@ -2,7 +2,8 @@ const homeController = require('../app/http/controllers/homeController');
 const authController = require('../app/http/controllers/authController');
 const cartController = require('../app/http/controllers/customers/cartController');
 const orderController = require('../app/http/controllers/customers/orderController');
-const AdminOrderController = require('../app/http/controllers/admin/orderController');
+const adminOrderController = require('../app/http/controllers/admin/orderController');
+const statusController = require('../app/http/controllers/admin/statusController');
 
 // MIDDLEWARES
 const guest = require('../app/http/middlewares/guest');
@@ -35,7 +36,8 @@ function initRoutes(app) {
     app.get('/customer/orders', auth, orderController().index);
 
     // ADMIN ROUTES
-    app.get('/admin/orders', admin, AdminOrderController().index);
+    app.get('/admin/orders', admin, adminOrderController().index);
+    app.post('/admin/order/status', admin, statusController().update);
 };
 
 module.exports = initRoutes;
